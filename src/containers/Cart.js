61b322d7d58ddaab407104545f9b686105318abc
Louisa Lee <louisa@Louisas-MacBook-Pro.local>
2018-02-24 04:25:15 +0800

    add address

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Products from '../components/cart/Products'
 import Summary from '../components/cart/Summary'
+import constants from '../constants/constants'
 import styled from 'styled-components'
 
 class Cart extends Component {
@@ -15,6 +16,15 @@ class Cart extends Component {
     return (
       <Container>
         <LeftContainer>
+          <AddressDiv>
+            <h2>Shipping Address</h2>
+            <Address>
+              <b>Smile Person</b><br />
+              2 Stamford Road<br />
+              Level 70 Equinox Complex, Downtown Core<br />
+              Singapore 178882<br />
+            </Address>
+          </AddressDiv>
           <Products products={products} cart={cart} />
         </LeftContainer>
 
@@ -42,6 +52,21 @@ const Container = styled.main`
 const LeftContainer = styled.div`
   width: 60%;
   height: 100%;
+  display: flex;
+  flex-flow: column;
+`
+
+const AddressDiv = styled.div`
+  width: 100%;
+  padding: ${constants.size.padding.large};
+`
+
+const Address = styled.div`
+  border: 1px solid black;
+  border-radius: ${constants.size.borderRadius.small};
+  font-family: 'Muli';
+  padding: ${constants.size.padding.medium};
+  margin-top: ${constants.size.margin.small};
 `
 
 const SummaryContainer = styled.div`
