f6c60d1e2af24a6ac9351a0e27c58b2806656856
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2018-02-23 20:23:28 +0800

    add containers

---
+++ src/containers/Catalogue.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Catalogue extends Component {
+
+  render() {
+    return (
+      <div>Catalogue Page</div>
+    );
+  }
+
+}
+
+export default Catalogue;
---
+++ src/containers/Checkout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+
+  render() {
+    return (
+      <div>Checkout Page</div>
+    );
+  }
+
+}
+
+export default Checkout;
